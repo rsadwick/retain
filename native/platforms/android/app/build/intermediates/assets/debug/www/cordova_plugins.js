b61d505765808444efb6dbb37d1d1eede2208973
Ryan Sadwick <rsadwick@gmail.com>
2018-05-31 14:39:44 -0400

    fix package name of project

--- native/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
+++ native/platforms/android/app/build/intermediates/assets/debug/www/cordova_plugins.js
@@ -1,6 +1,14 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
   {
+    "id": "cordova-plugin-device.device",
+    "file": "plugins/cordova-plugin-device/www/device.js",
+    "pluginId": "cordova-plugin-device",
+    "clobbers": [
+      "device"
+    ]
+  },
+  {
     "id": "cordova-plugin-file.DirectoryEntry",
     "file": "plugins/cordova-plugin-file/www/DirectoryEntry.js",
     "pluginId": "cordova-plugin-file",
@@ -188,25 +196,17 @@ module.exports = [
     "merges": [
       "window.plugins.speechRecognition"
     ]
-  },
-  {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
-    "clobbers": [
-      "device"
-    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-console": "1.1.0",
+  "cordova-plugin-device": "2.0.2",
   "cordova-plugin-file": "6.0.1",
   "cordova-plugin-http": "1.2.0",
   "cordova-plugin-speechrecognition": "1.1.2",
-  "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-device": "2.0.2"
+  "cordova-plugin-whitelist": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
