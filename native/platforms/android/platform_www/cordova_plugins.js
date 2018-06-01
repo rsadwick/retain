0493ed69436cdd6d001d62ecd00f2769fd83199e
Ryan Sadwick <rsadwick@gmail.com>
2018-06-01 15:53:29 -0400

    splash screen for android

--- native/platforms/android/platform_www/cordova_plugins.js
+++ native/platforms/android/platform_www/cordova_plugins.js
@@ -205,6 +205,14 @@ module.exports = [
       "cordova.InAppBrowser.open",
       "window.open"
     ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -216,7 +224,8 @@ module.exports.metadata =
   "cordova-plugin-http": "1.2.0",
   "cordova-plugin-speechrecognition": "1.1.2",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-inappbrowser": "3.0.0"
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-plugin-splashscreen": "5.0.2"
 };
 // BOTTOM OF METADATA
 });
--- native/platforms/ios/www/cordova_plugins.js
+++ native/platforms/ios/www/cordova_plugins.js
@@ -205,6 +205,14 @@ module.exports = [
       "cordova.InAppBrowser.open",
       "window.open"
     ]
+  },
+  {
+    "id": "cordova-plugin-splashscreen.SplashScreen",
+    "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+    "pluginId": "cordova-plugin-splashscreen",
+    "clobbers": [
+      "navigator.splashscreen"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -215,7 +223,8 @@ module.exports.metadata =
   "cordova-plugin-speechrecognition": "1.1.2",
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-plugin-device": "2.0.2",
-  "cordova-plugin-inappbrowser": "3.0.0"
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-plugin-splashscreen": "5.0.2"
 };
 // BOTTOM OF METADATA
 });
